de2c3b8662e202a43258a834100f58e5648ee1d1
Hamzah <hamzahnafalah@gmail.com>
2025-04-15 01:15:36 +0700

    update modules

--- src/Contracts/Schemas/Service.php
+++ src/Contracts/Schemas/Service.php
@@ -2,21 +2,19 @@
 
 namespace Hanafalah\ModuleService\Contracts\Schemas;
 
+use Hanafalah\LaravelSupport\Contracts\Data\PaginateData;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 interface Service extends DataManagement
 {
-    public function addOrChange(?array $attributes = []): self;
-    public function get(mixed $conditionals = null): Collection;
-    public function refind(mixed $id = null): Model|null;
-    public function service(mixed $conditionals = null): Builder;
     public function commonService(array $morphs, mixed $conditionals = null): Builder;
-    public function prepareViewServicePaginate(mixed $cache_reference_type, ?array $morphs = null, int $perPage = 50, array $columns = ['*'], string $pageName = 'page', ?int $page = null, ?int $total = null): LengthAwarePaginator;
+    public function prepareViewServicePaginate(mixed $cache_reference_type, ?array $morphs = null, PaginateData $paginate_dto): LengthAwarePaginator;
+    public function viewServicePaginate(mixed $cache_reference_type, ?array $morphs = null, ? PaginateData $paginate_dto = null): array;
     public function prepareViewServiceList(mixed $cache_reference_type, ?array $morphs = null): Collection;
-    public function viewServicePaginate(mixed $reference_type, ?array $morphs = null, int $perPage = 50, array $columns = ['*'], string $pageName = 'page', ?int $page = null, ?int $total = null): array;
-    public function viewServiceList(mixed $reference_type, ?array $morphs = null): array;
+    public function viewServiceList(mixed $cache_reference_type, ?array $morphs = null): array;
+    public function service(mixed $conditionals = null): Builder;
+    
 }
