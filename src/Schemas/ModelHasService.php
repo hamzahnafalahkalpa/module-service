bc38df4c444984f4e14c7371b2cd67eea7ed7fa7
Hamzah <hamzahnafalah@gmail.com>
2025-04-28 10:11:08 +0700

    update modules

--- src/Schemas/ModelHasService.php
+++ src/Schemas/ModelHasService.php
@@ -3,9 +3,6 @@
 namespace Hanafalah\ModuleService\Schemas;
 
 use Hanafalah\ModuleService\Contracts\Schemas\ModelHasService as ContractsServiceItem;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Collection;
 use Hanafalah\LaravelSupport\Supports\PackageManagement;
 
 class ModelHasService extends PackageManagement implements ContractsServiceItem
